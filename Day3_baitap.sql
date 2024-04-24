19dee0c391e8b422f6696467113078bd1ffc7280
nguyenthiphuongthao013 <168000540+nguyenthiphuongthao013@users.noreply.github.com>
2024-04-24 23:31:57 +0700

    Update Day3_baitap.sql

--- Day3_baitap.sql
+++ Day3_baitap.sql
@@ -19,3 +19,28 @@ where city not like 'a%' and city not like 'e%' and city not like 'i%' and city 
 select name from employee
 order by name
 ---baitap8
+select name from employee
+where salary>2000 AND months <10
+order by employee_id
+---baitap9
+select product_id from Products
+where low_fats = 'Y' AND recyclable = 'Y'
+---baitap10
+select name from customer
+where referee_id !='2'
+---baitap11
+select product_id from Products
+where low_fats = 'Y' AND recyclable = 'Y'
+---baitap12
+select distinct author_id as id from views
+where article_id>1
+order by author_id ASC
+---baitap13
+select part from parts_assembly
+where finish_date is NULL
+---BAITAP14
+select advertising_channel From uber_advertising
+where money_spent > 100000 and YEAR IN (2019)
+---baitap15
+select advertising_channel From uber_advertising
+where money_spent > 100000 and YEAR IN (2019)
