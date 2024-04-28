6d6f6ea265d8dd8d68b75237011dd7e9f9137802
nguyenthiphuongthao013 <168000540+nguyenthiphuongthao013@users.noreply.github.com>
2024-04-28 13:55:06 +0700

    Update Day5_baitap.sql

--- Day5_baitap.sql
+++ Day5_baitap.sql
@@ -29,4 +29,29 @@ FROM monthly_cards_issued
 GROUP BY card_name
 order by difference DEsc
 bai8
-
+SELECT manufacturer,
+Count (drug) as drug_count,
+ABS (sum(cogs-total_sales)) as Total_loss
+FROM pharmacy_sales
+where total_sales<cogs
+Group by manufacturer
+order by total_loss DESC
+bai9
+select * from cinema
+where id%2=1 and description != 'boring'
+order by rating DESC
+bai10
+select teacher_id,
+COUNT(distinct subject_id) as cnt
+from Teacher
+group by teacher_id
+BAI11
+SELECT DISTINCT user_id,
+COUNT(follower_id) AS followers_count
+FROM Followers
+GROUP BY user_id
+ORDER BY user_id
+BAI12
+SELECT CLASS FROM Courses
+GROUP BY CLASS
+HAVING COUNT(student)>5
