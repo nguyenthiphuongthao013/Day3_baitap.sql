596658ca8b086629dd4e16e53d5919fb4862b937
nguyenthiphuongthao013 <168000540+nguyenthiphuongthao013@users.noreply.github.com>
2024-05-07 20:50:24 +0700

    Update Day15_baitap.sql

--- Day15_baitap.sql
+++ Day15_baitap.sql
@@ -8,3 +8,16 @@ ROUND((spend-(LAG(spend)OVER(PARTITION BY product_id ORDER BY transaction_date))
 FROM user_transactions
 ORDER BY product_id
 Bai2
+SELECT  
+DISTINCT card_name,
+FIRST_VALUE(issued_amount) OVER(PARTITION BY card_name ORDER BY issue_year, issue_month) as issued_amount
+FROM monthly_cards_issued
+ORDER BY issued_amount DESC
+Bai 3
+SELECT user_id, 	spend, transaction_date FROM
+(
+SELECT user_id, 	spend, transaction_date,
+RANK() OVER(PARTITION BY user_id ORDER BY transaction_date ) AS M 
+FROM transactions) AS s 
+WHERE M = 3
+Bai 4
