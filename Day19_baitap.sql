7061f9af6faaacec6c8f617011c400532467e3fb
nguyenthiphuongthao013 <168000540+nguyenthiphuongthao013@users.noreply.github.com>
2024-05-14 23:03:00 +0700

    Update Day19_baitap.sql

--- Day19_baitap.sql
+++ Day19_baitap.sql
@@ -12,4 +12,22 @@ select * from public.sales_dataset_rfm_prj
 where ORDERNUMBER is null and QUANTITYORDERED is null 
 and PRICEEACH is null and ORDERLINENUMBER is null and SALES is null and ORDERDATE is null;
 Bai3
+ALTER TABLE public.sales_dataset_rfm_prj
+ADD COLUMN CONTACTLASTNAME VARCHAR
+ALTER TABLE public.sales_dataset_rfm_prj
+ADD COLUMN CONTACTFIRSTNAME VARCHAR
+UPDATE public.sales_dataset_rfm_prj
+SET CONTACTLASTNAME = substring(contactfullname FROM 1 FOR POSITION('-' IN contactfullname)-1)
+UPDATE public.sales_dataset_rfm_prj
+SET CONTACTFIRSTNAME = substring(contactfullname FROM POSITION('-' IN contactfullname)+1)
+Bai4
+ALTER TABLE public.sales_dataset_rfm_prj
+ADD COLUMN QTR_ID DATE,
+ADD COLUMN MONTH_ID DATE,
+ADD COLUMN YEAR_ID DATE
+Bai5
 
+Bai6
+ALTER TABLE public.sales_dataset_rfm_prj
+RENAME TO SALES_DATASET_RFM_PRJ_CLEAN
+  
