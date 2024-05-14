abaf853f791d0cc61791acc7760c9c7b415f4cb8
nguyenthiphuongthao013 <168000540+nguyenthiphuongthao013@users.noreply.github.com>
2024-05-14 21:22:03 +0700

    Update Day19_baitap.sql

--- Day19_baitap.sql
+++ Day19_baitap.sql
@@ -1,1 +1,11 @@
+Bai1
+ALTER TABLE public.sales_dataset_rfm_prj
+ALTER COLUMN priceeach TYPE numeric USING (trim(priceeach)::numeric),
+ALTER COLUMN ordernumber TYPE numeric USING (trim(ordernumber)::numeric),
+ALTER COLUMN quantityordered TYPE numeric USING (trim(quantityordered)::numeric),
+ALTER COLUMN orderlinenumber TYPE numeric USING (trim(orderlinenumber)::numeric),
+ALTER COLUMN sales TYPE numeric USING (trim(sales)::numeric),
+ALTER COLUMN msrp TYPE numeric USING (trim(msrp)::numeric),
+ALTER COLUMN orderdate TYPE TIMESTAMP USING (trim(orderdate)::TIMESTAMP);
+Bai2
 
