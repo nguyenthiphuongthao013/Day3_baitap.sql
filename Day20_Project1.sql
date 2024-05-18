cfb11f337576c72e1453def9a9fc2aba726080e8
nguyenthiphuongthao013 <168000540+nguyenthiphuongthao013@users.noreply.github.com>
2024-05-18 22:39:27 +0700

    Update Day20_Project1.sql

--- Day20_Project1.sql
+++ Day20_Project1.sql
@@ -1,1 +1,12 @@
+Bai1
+select 
+extract(year_month from created_at),
+count(order_id) as total_order, 
+count(distinct user_id) as total_user
+from bigquery-public-data.thelook_ecommerce.orders
+where created_at between '2019-01-1' and '2022-04-30'
+group by 1
+order by 1
+
+Bai2
 
