ecda92a5b77a7104ada0d06b0cdaf876cd4a6090
nguyenthiphuongthao013 <168000540+nguyenthiphuongthao013@users.noreply.github.com>
2024-05-19 19:40:27 +0700

    Update Day20_Project1.sql

--- Day20_Project1.sql
+++ Day20_Project1.sql
@@ -1,12 +1,18 @@
+Ad-hoc tasks
 Bai1
 select 
-extract(year_month from created_at),
-count(order_id) as total_order, 
-count(distinct user_id) as total_user
-from bigquery-public-data.thelook_ecommerce.orders
-where created_at between '2019-01-1' and '2022-04-30'
+Format_date('%Y-%m', I.delivered_at) as Year_month,
+count(O.order_id) as total_order,
+count(distinct O.user_id) as total_user
+from bigquery-public-data.thelook_ecommerce.orders as O
+join bigquery-public-data.thelook_ecommerce.order_items as I 
+on O.order_id=I.order_id
+where O.status = 'Complete' and I.delivered_at between '2019-01-01 00:00:00' and '2022-05-01 00:00:00'
 group by 1
 order by 1
-
+  
+Insight: 
+- Có sự tăng đều qua các tháng, năm
+  
 Bai2
 
